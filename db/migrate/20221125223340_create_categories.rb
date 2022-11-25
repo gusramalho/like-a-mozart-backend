aeda3d1e1295215895844255029a1b8da9beed42
Gustavo Ramalho <gugarroliver@hotmail.com>
2022-11-25 19:34:44 -0300

    Add model Category

---
+++ db/migrate/20221125223340_create_categories.rb
@@ -0,0 +1,10 @@
+class CreateCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories, id: :uuid do |t|
+      t.string :name
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
